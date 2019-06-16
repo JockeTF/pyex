1363b35a66b11c2a2003656b54cacd436ff75deb
Joakim Soderlund <joakim.soderlund@gmail.com>
2019-06-16 11:11:41 +0200

    Expand the description in exercise 002

--- pyex/e002/main.py
+++ pyex/e002/main.py
@@ -4,7 +4,12 @@
 """
 002 Simple Recursion
 
-Demonstrates how the stack is used in recursion.
+Demonstrates how the stack is used in recursion. Run the program in a
+debugger (as described below). Step through the program and watch how
+the stack grows and shrinks when functions are called and returns. You
+may also want to inspect the different variables values in each stack
+frame. Also, try changing the initial recurse call and watch how that
+affects the number of frames on the stack.
 
 Running the program in the debugger:
     $ pudb3 pyex/e002/main.py
